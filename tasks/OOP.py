5268add78bf5914185981ae610837262fb53aff0
Koneko35 <andermak007@gmail.com>
2024-04-05 19:47:15 +0300

    OOP

--- tasks/OOP.py
+++ tasks/OOP.py
@@ -33,18 +33,72 @@
 
 # zadanie 3
 
-class Song:
-    def __init__(self, title, artist, release_year) -> None:
-        self.title = title
-        self.artist = artist
-        self.release_year = release_year
+# class Song:
+#     def __init__(self, title, artist, release_year) -> None:
+#         self.title = title
+#         self.artist = artist
+#         self.release_year = release_year
+#
+#     def introduce(self):
+#         print('Какая-то песня',self.title , 'Кто-то', self.artist, self.release_year)
+#
+#     def __str__(self):
+#         string_info = str(self.__dict__)
+#         return string_info
+#
+# some_song = Song('Manabreak', 'ZXCursed', '2022')
+# print(some_song)
 
-    def introduce(self):
-        print('Какая-то песня',self.title , 'Кто-то', self.artist, self.release_year)
+# zadanie 4
+# class Podcast_Episode:
+#     def __init__(self, title, artist, duration, guest):
+#         self.title = title
+#         self.artist = artist
+#         self.guest = guest
+#         self.o3m = self.co3m(duration)
+#
+#     def co3m(self, duration):
+#         if duration >= 30:
+#             return True
+#
+#     def __str__(self):
+#         string_info = str(self.__dict__)
+#         return string_info
+#
+#
+# some_podcast = Podcast_Episode ('Какой-то подкаст', 'Кто-то', 123456, 'Кто-то другой')
+# print(some_podcast)
+
+# zadanie 5 6
+
+class Audio_Item:
+    def __init__(self,title, artist):
+        self.artist = artist
+        self.title = title
 
     def __str__(self):
-        string_info = self.title, self.artist, self.release_year
+        string_info = str(self.__dict__)
         return string_info
+
+class Song(Audio_Item):
+        def __init__(self, title, artist, release_year):
+            super().__init__(title, artist)
+            self.release_year = release_year
+
+class Podcast_Episode(Audio_Item):
+    def __init__(self, title, artist, duration, guest):
+        super().__init__(title, artist)
+        self.duration = duration
+        self.guest = guest
+        self.o3m = self.co3m(duration)
+
+    def co3m(self, duration):
+        if duration >= 30:
+            return True
+
 
 some_song = Song('Manabreak', 'ZXCursed', '2022')
 print(some_song)
+
+some_podcast = Podcast_Episode ('Какой-то подкаст', 'Кто-то', 123456, 'Кто-то другой')
+print(some_podcast)
