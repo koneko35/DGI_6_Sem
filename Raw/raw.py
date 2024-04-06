f4995696220c2479862775b8b80d29e9030f8f39
andrey <andermak007@gmail.com>
2024-04-06 13:15:24 +0300

    a

---
+++ Raw/raw.py
@@ -0,0 +1,42 @@
+import matplotlib.pyplot as np
+fileobj =  open("sig.wav" , mode="rb")
+
+data = fileobj.read()
+
+# число бит в сигнале
+file_size_inbytes = data[4:8]
+print(file_size_inbytes)
+
+# число байт в сигнале
+file_size = int.from_bytes(file_size_inbytes, byteorder= 'little')
+print(file_size)
+
+# колличество каналов
+num_of_channel_in_inbytes = data[22:24]
+num_of_channel = int.from_bytes(num_of_channel_in_inbytes, byteorder= 'little')
+print(num_of_channel)
+
+# значение аудио файла в байтах
+audio_data_size_in_inbytes = data[40:44]
+audio_data_size = int.from_bytes(audio_data_size_in_inbytes, byteorder= 'little')
+print(audio_data_size)
+
+
+# частота дикретизации
+sampl_rate_in_bytes = data[24:28]
+sampl_rate = int.from_bytes(sampl_rate_in_bytes, byteorder='little')
+
+# амплитуда сигнала в разные моменты времени
+music_amps = []
+
+for i in range(0,audio_data_size, 2):
+    amp_in_bytes = data[44+i:44+i+2]
+    amp = int.from_bytes(amp_in_bytes, byteorder= 'little', signed=True)
+    music_amps.append(amp)
+
+xdata = range(0, len(music_amps)/sampl_rate, 1/sampl_rate)
+
+np.plot(range(len(music_amps))/sampl_rate, music_amps)
+np.show()
+
+pass
